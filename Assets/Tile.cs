a60fbdd75b0b5b60870dbb634e453bf6d44d0079
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-12-15 14:13:15 -0800

    start sketching the game out

---
+++ Assets/Tile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool IsMine { get; private set; } = false;
+    public IEnumerable<Tile> Neighbors;
+    public int NeighborMineCount => Neighbors.Where(x => x.IsMine).Count();
+
+    public void OnClicked()
+    {
+        Reveal();
+    }
+    public void Reveal()
+    {
+        if (IsMine)
+        {
+            Game.Over();
+            return;
+        }
+        if(NeighborMineCount > 0)
+        {
+            // change texture, include number
+            return;
+        }
+        foreach (Tile t in Neighbors)
+        {
+            t.Reveal();
+        }
+    }
+}
