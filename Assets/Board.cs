cc3fe1e93c10e646354c6ad8fcbcc4b28d1b22d5
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-01-10 12:08:48 -0800

    mines now placed randomly

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class Board
 {
     const float CAMERA_SIZE_FACTOR = 1.1f;
-    private List<List<Tile>> _board = new List<List<Tile>>();
+    private readonly List<List<Tile>> _board = new List<List<Tile>>();
     public IEnumerable<Tile> AllTiles
     {
         get
@@ -16,20 +16,42 @@ public class Board
             foreach (List<Tile> l in _board) foreach (Tile t in l) yield return t;
         }
     }
-    public int MinesRemaining => AllTiles.Where(x => x.Flag == Tile.FlagStatus.Flagged).Count();
-    public Board(int xDimension, int yDimension)
+    public static IEnumerable<(int x, int y)> AllCoordinatesForDimension((int x, int y) dim)
     {
-        for(int i = 0; i < xDimension; i++)
+        (int x, int y) = dim;
+        for (int i = 0; i < x; i++) for (int j = 0; j < y; j++) yield return (i, j);
+    }
+    private static IEnumerable<(int x, int y)> MineCoordinateEnumerable((int x, int y) dim, int numMines)
+    {
+        List<(int x, int y)> AllCoords = AllCoordinatesForDimension(dim).ToList();
+        for(int i = 0; i < numMines; i++)
+        {
+            if (AllCoords.Any())
+            {
+                (int x, int y) item = AllCoords[Game.Random.Next(0, AllCoords.Count)];
+                yield return item;
+                AllCoords.Remove(item);
+            }
+            else break;
+        }
+    }
+    public static HashSet<(int x, int y)> MineCoordinateSet((int x, int y) dim, int numMines) => new HashSet<(int x, int y)>(MineCoordinateEnumerable(dim, numMines));
+    public int MinesRemaining => AllTiles.Where(x => x.Flag == Tile.FlagStatus.Flagged).Count();
+    public Board((int x, int y) dimensions, int numMines)
+    {
+        HashSet<(int x, int y)> MineCoords = MineCoordinateSet(dimensions, numMines);
+        (int x, int y) = dimensions;
+        for(int i = 0; i < x; i++)
         {
             _board.Add(new List<Tile>());
-            for(int j = 0; j < yDimension; j++)
+            for(int j = 0; j < y; j++)
             {
                 Tile newTile = GameObject.Instantiate(Game.TileObject, new Vector3(i, j), Quaternion.identity).GetComponent<Tile>();
                 _board[i].Add(newTile);
-                newTile.Init(i, j, (i * j) % 7 == 0);
+                newTile.Init(i, j, MineCoords.Contains((i, j)));
             }
         }
-        CenterBoardInCamera(xDimension, yDimension);
+        CenterBoardInCamera(x, y);
     }
     public Tile this[int x, int y]
     {
--- Assets/Game.cs
+++ Assets/Game.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public static (int x, int y) BoardSize = (80, 45);
+    public static int NumMines = 10;
     public static Game Instance = null;
     public static Board Board;
+    public static System.Random Random = new System.Random();
     public static GameObject TileObject => Instance._tileObject;
     [SerializeField]
     private GameObject _tileObject;
@@ -21,7 +24,7 @@ public class Game : MonoBehaviour
             return;
         }
         Instance = this;
-        Board = new Board(10, 10);
+        Board = new Board(BoardSize, NumMines);
     }
     // Update is called once per frame
     void Update()
